9487e757760dcff57a374e357600decc7bf9ed62
Lalu KismaraHadi <n.kismara@gmail.com>
2026-01-17 03:17:38 +0700

    feat: add Google AdSense script to the application head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -56,6 +56,13 @@ export default defineNuxtConfig({
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap' },
       ],
+      script: [
+        {
+          src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2104604669547614',
+          async: true,
+          crossorigin: 'anonymous',
+        },
+      ],
     },
   },
 
